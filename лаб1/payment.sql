a7983a75d85f7fab50697b3aa5b82c2a0855d28a
Rev <pvttys@mail.ru>
2019-10-25 10:46:07 +0500

    changes bd

--- лаб1/payment.sql
+++ лаб1/payment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 15 2019 г., 10:48
+-- Время создания: Окт 25 2019 г., 08:42
 -- Версия сервера: 10.3.13-MariaDB
 -- Версия PHP: 7.1.22
 
@@ -29,13 +29,43 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `accounts` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `type_account_id` int(11) NOT NULL,
-  `frozen` tinyint(1) DEFAULT NULL,
-  `balance` float NOT NULL,
-  `created_at` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `type_account_id` int(10) UNSIGNED NOT NULL,
+  `type_credit_id` int(10) UNSIGNED DEFAULT NULL,
+  `frozen` tinyint(1) NOT NULL,
+  `balance` double(8,2) NOT NULL,
+  `validity_period` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2019_10_25_043504_create_accounts_table', 1),
+(3, '2019_10_25_044417_create_type_accounts_table', 1),
+(4, '2019_10_25_044909_add_type_account_id_to_accounts_table', 1),
+(5, '2019_10_25_045338_create_payments_table', 1),
+(6, '2019_10_25_051949_create_type_autopayments_table', 1),
+(7, '2019_10_25_052132_add_type_autopayment_id_to_payments_table', 1),
+(8, '2019_10_25_053136_create_type_credits_table', 1),
+(9, '2019_10_25_053704_add_type_credit_id_to_accounts_table', 1);
 
 -- --------------------------------------------------------
 
@@ -44,14 +74,17 @@ CREATE TABLE `accounts` (
 --
 
 CREATE TABLE `payments` (
-  `id` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `sender_id` int(11) NOT NULL,
-  `recipient_id` int(11) NOT NULL,
-  `transactions` float NOT NULL,
-  `autopayment` tinyint(1) DEFAULT NULL,
-  `dateAutopayment` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(10) UNSIGNED NOT NULL,
+  `sender_id` int(10) UNSIGNED NOT NULL,
+  `recipient_id` int(10) UNSIGNED NOT NULL,
+  `transactions` double(8,2) NOT NULL,
+  `auto_payment` tinyint(1) NOT NULL,
+  `type_autopayment_id` int(10) UNSIGNED DEFAULT NULL,
+  `date_auto_payment` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -60,12 +93,36 @@ CREATE TABLE `payments` (
 --
 
 CREATE TABLE `type_accounts` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `validity_period` int(11) NOT NULL,
-  `credit_limit` float DEFAULT NULL,
-  `condition` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `type_autopayments`
+--
+
+CREATE TABLE `type_autopayments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `type_credits`
+--
+
+CREATE TABLE `type_credits` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `percent` double(8,2) NOT NULL,
+  `condition` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `limit_credit` double(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -74,12 +131,16 @@ CREATE TABLE `type_accounts` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `fio` varchar(50) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `series` varchar(10) NOT NULL,
-  `number` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `series` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Индексы сохранённых таблиц
@@ -90,16 +151,24 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `type_account_id` (`type_account_id`),
-  ADD KEY `accounts_ibfk_1` (`user_id`);
+  ADD KEY `accounts_user_id_foreign` (`user_id`),
+  ADD KEY `accounts_type_account_id_foreign` (`type_account_id`),
+  ADD KEY `accounts_type_credit_id_foreign` (`type_credit_id`);
+
+--
+-- Индексы таблицы `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `payments`
 --
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `sender_id` (`sender_id`),
-  ADD KEY `recipient_id` (`recipient_id`);
+  ADD KEY `payments_sender_id_foreign` (`sender_id`),
+  ADD KEY `payments_recipient_id_foreign` (`recipient_id`),
+  ADD KEY `payments_type_autopayment_id_foreign` (`type_autopayment_id`);
 
 --
 -- Индексы таблицы `type_accounts`
@@ -108,10 +177,23 @@ ALTER TABLE `type_accounts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `type_autopayments`
+--
+ALTER TABLE `type_autopayments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `type_credits`
+--
+ALTER TABLE `type_credits`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -121,25 +203,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `type_accounts`
 --
 ALTER TABLE `type_accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `type_autopayments`
+--
+ALTER TABLE `type_autopayments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `type_credits`
+--
+ALTER TABLE `type_credits`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -149,15 +249,17 @@ ALTER TABLE `users`
 -- Ограничения внешнего ключа таблицы `accounts`
 --
 ALTER TABLE `accounts`
-  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`type_account_id`) REFERENCES `type_accounts` (`id`);
+  ADD CONSTRAINT `accounts_type_account_id_foreign` FOREIGN KEY (`type_account_id`) REFERENCES `type_accounts` (`id`),
+  ADD CONSTRAINT `accounts_type_credit_id_foreign` FOREIGN KEY (`type_credit_id`) REFERENCES `type_credits` (`id`),
+  ADD CONSTRAINT `accounts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `payments`
 --
 ALTER TABLE `payments`
-  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `accounts` (`id`),
-  ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`recipient_id`) REFERENCES `accounts` (`id`);
+  ADD CONSTRAINT `payments_recipient_id_foreign` FOREIGN KEY (`recipient_id`) REFERENCES `accounts` (`id`),
+  ADD CONSTRAINT `payments_sender_id_foreign` FOREIGN KEY (`sender_id`) REFERENCES `accounts` (`id`),
+  ADD CONSTRAINT `payments_type_autopayment_id_foreign` FOREIGN KEY (`type_autopayment_id`) REFERENCES `type_autopayments` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
